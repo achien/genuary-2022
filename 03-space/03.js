77d24af0729774b18229303aa6bdd8d4d55351d8
Andrew Chien <aechien@gmail.com>
2022-01-03 22:35:51 -0500

    meteor shower

--- 03-space/03.js
+++ 03-space/03.js
@@ -10,7 +10,29 @@ const MAX_STAR_DIAMETER = 8;
 // The middle of the star is a solid circle, the outside is fuzzy
 const STAR_POINT_DIAMETER = 1.5;
 
-const stars = Array(NUM_STARS);
+// pixels/second
+const MAX_METEOR_DX = 100;
+const MIN_METEOR_DY = 50;
+const MAX_METEOR_DY = 200;
+// milliseconds
+const MIN_METEOR_DURATION = 2000;
+const MAX_METEOR_DURATION = 5000;
+const METEOR_FADE_IN = 250;
+const METEOR_FADE_OUT = 250;
+
+const METEOR_DIAMETER = 10;
+const METEOR_COLOR = "#fff";
+// in ms of travel time
+const METEOR_TRAIL_LENGTH = 250;
+
+let startTime;
+let lastMeteorCreated;
+let stars = Array(NUM_STARS);
+let meteors = [];
+
+function randNum(min, max) {
+  return min + Math.random() * (max - min);
+}
 
 function pickStarColor() {
   const random = Math.random();
@@ -33,20 +55,23 @@ function setup() {
   createCanvas(w, h);
 
   for (let i = 0; i < NUM_STARS; i++) {
-    const x = Math.random() * w;
-    const y = Math.random() * h;
-    const diameter =
-      MIN_STAR_DIAMETER +
-      Math.random() * (MAX_STAR_DIAMETER - MIN_STAR_DIAMETER);
-    const color = pickStarColor();
-    stars[i] = { x, y, diameter, color };
+    stars[i] = {
+      x: Math.random() * w,
+      y: Math.random() * h,
+      diameter: randNum(MIN_STAR_DIAMETER, MAX_STAR_DIAMETER),
+      color: pickStarColor(),
+    };
   }
+
+  startTime = performance.now();
+  lastMeteorCreated = startTime;
 }
 
 function drawStar(star) {
   const { x, y, diameter, color: starColor } = star;
-  // Small stars are circles
+  noStroke();
   if (diameter <= STAR_POINT_DIAMETER) {
+    // Small stars are circles
     fill(starColor);
     circle(x, y, diameter);
   } else {
@@ -66,13 +91,92 @@ function drawStar(star) {
   }
 }
 
+function makeMeteor(now) {
+  return {
+    created: now,
+    x: Math.random() * w,
+    y: Math.random() * h,
+    dx: randNum(-MAX_METEOR_DX, MAX_METEOR_DX),
+    dy: randNum(MIN_METEOR_DY, MAX_METEOR_DY),
+    duration: randNum(MIN_METEOR_DURATION, MAX_METEOR_DURATION),
+  };
+}
+
+function drawMeteor(meteor, now) {
+  const { created, x: initialX, y: initialY, dx, dy, duration } = meteor;
+  const x = initialX + (dx * (now - created)) / 1000;
+  const y = initialY + (dy * (now - created)) / 1000;
+
+  const elapsed = now - created;
+  const remaining = duration - elapsed;
+  let meteorColor = color(METEOR_COLOR);
+  if (elapsed < METEOR_FADE_IN) {
+    meteorColor = lerpColor(
+      color(SPACE_COLOR),
+      color(METEOR_COLOR),
+      elapsed / METEOR_FADE_IN
+    );
+  } else if (remaining < METEOR_FADE_OUT) {
+    meteorColor = lerpColor(
+      color(SPACE_COLOR),
+      color(METEOR_COLOR),
+      remaining / METEOR_FADE_OUT
+    );
+  }
+  drawStar({
+    x,
+    y,
+    diameter: METEOR_DIAMETER,
+    color: meteorColor,
+  });
+
+  // Draw the trail
+  const startTime = now - Math.min(METEOR_TRAIL_LENGTH, elapsed);
+  const startX = initialX + (dx * (startTime - created)) / 1000;
+  const startY = initialY + (dy * (startTime - created)) / 1000;
+
+  // Draw segments to make a gradient
+  const SEGMENTS = 10;
+  for (let i = 1; i <= SEGMENTS; i++) {
+    const segmentColor = lerpColor(
+      color(SPACE_COLOR),
+      color(meteorColor),
+      i / SEGMENTS
+    );
+    stroke(segmentColor);
+    line(
+      startX + ((x - startX) * (i - 1)) / SEGMENTS,
+      startY + ((y - startY) * (i - 1)) / SEGMENTS,
+      startX + ((x - startX) * i) / SEGMENTS,
+      startY + ((y - startY) * i) / SEGMENTS
+    );
+  }
+}
+
 function draw() {
   background(SPACE_COLOR);
 
-  noStroke();
   for (const star of stars) {
     drawStar(star);
   }
 
-  noLoop();
+  // Prune all stale meteors
+  const now = performance.now();
+  meteors = meteors.filter((m) => now - m.created <= m.duration);
+
+  // Create a meteor if necessary. arctan is arbitrary; I want a function that
+  // starts at 0 and is 1 at infinity to allow (theoretically) for arbitrarily
+  // large gaps in meteors.
+  const timeSinceLastMeteor = now - lastMeteorCreated;
+  const shouldCreate =
+    (PI / 2) * Math.random() < Math.atan(timeSinceLastMeteor / 30000);
+  if (shouldCreate) {
+    const meteor = makeMeteor(now);
+    lastMeteorCreated = now;
+    meteors.push(meteor);
+  }
+
+  for (const meteor of meteors) {
+    drawMeteor(meteor, now);
+  }
 }
